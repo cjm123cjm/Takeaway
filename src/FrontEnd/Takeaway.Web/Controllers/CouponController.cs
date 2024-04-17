4451164189c498278cbf146e31b877454b9f5a86
chenjiamei <2671433141@qq.com>
2024-04-17 14:32:56 +0800

    coupon web

---
+++ src/FrontEnd/Takeaway.Web/Controllers/CouponController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Takeaway.Web.Models;
+using Takeaway.Web.Service.IService;
+
+namespace Takeaway.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new List<CouponDto>();
+            var response = await _couponService.GetCouponsAsync();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult CouponCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto couponDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _couponService.CreateCouponAsync(couponDto);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction("CouponIndex");
+                }
+                else
+                {
+                    ModelState.AddModelError("CouponCreate", response.Message);
+                }
+            }
+            return View(couponDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            CouponDto? list = new CouponDto();
+            var response = await _couponService.GetCouponByIdAsync(couponId);
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+                return View(list);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponDelete(CouponDto couponDto)
+        {
+            CouponDto? list = new CouponDto();
+            var response = await _couponService.DeleteCouponAsync(couponDto.CouponId);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CouponIndex));
+            }
+            ModelState.AddModelError("CouponDelete", "删除失败");
+            return View(couponDto);
+        }
+    }
+}
--- src/FrontEnd/Takeaway.Web/Models/CouponDto.cs
+++ src/FrontEnd/Takeaway.Web/Models/CouponDto.cs
@@ -1,10 +1,15 @@
-﻿namespace Takeaway.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Takeaway.Web.Models
 {
-	public class CouponDto
-	{
-		public int CouponId { get; set; }
-		public string CouponCode { get; set; } = null!;
-		public double DiscountAmount { get; set; }
-		public int MinAmount { get; set; }
-	}
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        [Required(ErrorMessage = "优惠码不能为空")]
+        public string CouponCode { get; set; } = null!;
+        public double DiscountAmount { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "最低数量大于0")]
+        public int MinAmount { get; set; }
+    }
 }
